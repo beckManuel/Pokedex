a3499fd496d67e14fbb03e8d9f50ddd8bc1d5aea
Diogo Bicho <diogo.bicho@tekever.com>
2020-08-06 18:43:20 +0100

    added splashscreen layout
    Signed-off-by: Diogo Bicho <diogo.bicho@tekever.com>

--- app/src/main/java/com/tekever/pokedex/presentation/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/tekever/pokedex/presentation/splash/SplashScreenFragment.kt
@@ -6,17 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.ActionOnlyNavDirections
 import androidx.navigation.fragment.findNavController
 import com.tekever.pokedex.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenFragment : Fragment() {
 
     companion object {
-        private const val TRANSITION_DELAY:Long = 2500
+        private const val TRANSITION_DELAY: Long = 3000
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
 
@@ -25,14 +32,11 @@ class SplashScreenFragment : Fragment() {
         goToMain()
     }
 
-    private fun goToMain(){
-
-        Handler().postDelayed({
+    private fun goToMain() {
+        activity?.lifecycleScope?.launch {
+            delay(TRANSITION_DELAY)
             findNavController().navigate(ActionOnlyNavDirections(R.id.action_splash_destination_to_home_destination))
-        },
-            TRANSITION_DELAY
-        )
 
-
+        }
     }
 }
